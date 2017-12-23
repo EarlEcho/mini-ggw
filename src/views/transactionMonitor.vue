0418e59cda5c3078aad273caeca6f8400c1a0e7b
EarEcho <734952325@qq.com>
2017-12-23 17:32:49 +0800

    增加  地图弹出框

--- src/views/transactionMonitor.vue
+++ src/views/transactionMonitor.vue
@@ -1,5 +1,5 @@
 <!--交易监控-->
-<style scoped lang="less">
+<style lang="less">
     .transaction-wrapper {
         width: 1920px;
         height: 950px;
@@ -61,6 +61,54 @@
             color: #fff;
         }
     }
+    .el-popover {
+        padding: 0;
+        border-radius: 0;
+    }
+
+    .map-click-chart {
+        width: 650px;
+        height: 390px;
+        position: absolute;
+        border: solid 1px #288ed9;
+        top: 25%;
+        left: 32%;
+        background: rgba(22, 45, 72, 0.8);
+        .title {
+            height: 40px;
+            line-height: 40px;
+            text-align: center;
+            color: white;
+            border-bottom: solid 1px #288ed9;
+        }
+        .chart-tabs {
+            height: 35px;
+            .el-tabs--card > .el-tabs__header {
+                border-bottom: none;
+            }
+            .el-tabs--card > .el-tabs__header .el-tabs__nav {
+                border: none;
+            }
+            .el-tabs__item {
+                border: none;
+                border-radius: 0;
+                background: transparent;
+                height: 34px;
+                line-height: 34px;
+                color: white;
+                width: 299px;
+                text-align: center;
+                border-bottom: solid 1px #288ed9;
+                &:first-child {
+                    border-right: solid 1px #288ed9;
+                }
+            }
+            .el-tabs__item.is-active {
+                border-bottom: none;
+            }
+
+        }
+    }
 </style>
 <template>
     <div>
@@ -100,6 +148,35 @@
                 <right-bottom-table></right-bottom-table>
             </div>
 
+            <!--点击地图的弹出框-->
+            <el-popover popper-class="map-click-chart" transition="el-zoom-in-center" v-model="showMapChart" ref="popover5"
+                        placement="top">
+                <div class="title">
+                    XX地区交易及价格信息
+                </div>
+                <div class="chart-tabs">
+                    <el-tabs v-model="chartActive" type="card">
+                        <!--<el-tab-pane label="交易数据分析" name="first"></el-tab-pane>-->
+                        <el-tab-pane label="价格信息分析" name="second"></el-tab-pane>
+                    </el-tabs>
+                </div>
+                <div class="checkbox-wrapper clearfix">
+                    <el-radio-group v-model="radioValue1">
+                        <el-radio :label="3">按量</el-radio>
+                        <el-radio :label="6">按金额</el-radio>
+                        <el-radio :label="9">按品种</el-radio>
+                    </el-radio-group>
+                    <el-radio-group v-model="radioValue2" class="g-rt">
+                        <el-radio :label="3">1月</el-radio>
+                        <el-radio :label="6">3月</el-radio>
+                        <el-radio :label="9">6月</el-radio>
+                    </el-radio-group>
+                </div>
+                <div id="map-innner-chart" style="width: 100%;height: 260px;">
+
+                </div>
+
+            </el-popover>
 
         </div>
     </div>
@@ -139,6 +216,11 @@
         props: [],
         data() {
             return {
+                /*地图弹出框*/
+                chartActive: 'first',
+                radioValue1: 3,
+                radioValue2: 3,
+                showMapChart: false,
                 /*位于地图上方的交易数据概述*/
                 transactionDatas: {
                     /*未出库，已出库，总交易金额，总交易量*/
@@ -211,6 +293,141 @@
                         value: 90
                     }]
                 ],
+                chartOption: {
+                    backgroundColor: '#172f4b',
+                    tooltip: {
+                        trigger: 'axis',
+                        axisPointer: {
+                            lineStyle: {
+                                color: '#57617B'
+                            }
+                        }
+                    },
+                    grid: {
+                        left: '0',
+                        right: '10px',
+                        top: '5px',
+                        bottom: '0',
+                        containLabel: true
+                    },
+                    xAxis: [{
+                        type: 'category',
+                        boundaryGap: false,
+                        axisLine: {
+                            lineStyle: {
+                                color: '#57617B'
+                            }
+                        },
+                        data:[]
+                    }],
+                    yAxis: [{
+                        type: 'value',
+                        axisTick: {
+                            show: false
+                        },
+                        axisLine: {
+                            lineStyle: {
+                                color: '#57617B'
+                            }
+                        },
+                        axisLabel: {
+                            margin: 5,
+                            textStyle: {
+                                fontSize: 14
+                            }
+                        },
+                        splitLine: {
+                            lineStyle: {
+                                color: '#57617B'
+                            }
+                        }
+                    }],
+                    series: [{
+                        name: '规格1',
+                        type: 'line',
+                        smooth: true,
+                        lineStyle: {
+                            normal: {
+                                width: 1
+                            }
+                        },
+                        areaStyle: {
+                            normal: {
+                                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                                    offset: 0,
+                                    color: 'rgba(137, 189, 27, 0.3)'
+                                }, {
+                                    offset: 0.8,
+                                    color: 'rgba(137, 189, 27, 0)'
+                                }], false),
+                                shadowColor: 'rgba(0, 0, 0, 0.1)',
+                                shadowBlur: 20
+                            }
+                        },
+                        itemStyle: {
+                            normal: {
+                                color: 'rgb(137,189,27)'
+                            }
+                        },
+                        data: ["3080", "4000", 0, 0, "3300", "3000.05", "3070", 0, "3090", "3040.00", "3040", "3050", 0, 0, 0, "3373.00", "3073.00", "3250", "2999", "3250", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "3250", "3350.00", 0, "3350", "3350", 0, "3350", "3350", "3350", "3471.00", "3472.11", "3472.11", "3488", "3511", "3555", "3555", 0, 0, 0, "3533", "3533", 0, "3566", "3566", "3566", "3400", "3400", 0, "3400"]
+                    }, {
+                        name: '规格2',
+                        type: 'line',
+                        smooth: true,
+                        lineStyle: {
+                            normal: {
+                                width: 1
+                            }
+                        },
+                        areaStyle: {
+                            normal: {
+                                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                                    offset: 0,
+                                    color: 'rgba(0, 136, 212, 0.3)'
+                                }, {
+                                    offset: 0.8,
+                                    color: 'rgba(0, 136, 212, 0)'
+                                }], false),
+                                shadowColor: 'rgba(0, 0, 0, 0.1)',
+                                shadowBlur: 20
+                            }
+                        },
+                        itemStyle: {
+                            normal: {
+                                color: 'rgb(0,136,212)'
+                            }
+                        },
+                        data: ["3090", "3080", "3950.1", "3987.3", "3907.3", "3020.05", "3010", "3090", "3080", "3030.00", "3030", "3030", 0, 0, 0, "3353.00", "3053.00", "3500", "3583", "3500", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "3500", "3600.00", 0, "3600", "3600", 0, "3600", "3600", "3600", "3721.00", "3722.11", "3722.11", "3700", "4212", "3555", "3555", 0, 0, 0, "3555", "3555", 0, "3555", "3555", "3555", "3555", "3555", 0, "3555"]
+                    }, {
+                        name: '规格3',
+                        type: 'line',
+                        smooth: true,
+                        lineStyle: {
+                            normal: {
+                                width: 1
+                            }
+                        },
+                        areaStyle: {
+                            normal: {
+                                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                                    offset: 0,
+                                    color: 'rgba(219, 50, 51, 0.3)'
+                                }, {
+                                    offset: 0.8,
+                                    color: 'rgba(219, 50, 51, 0)'
+                                }], false),
+                                shadowColor: 'rgba(0, 0, 0, 0.1)',
+                                shadowBlur: 20
+                            }
+                        },
+                        itemStyle: {
+                            normal: {
+                                color: 'rgb(219,50,51)'
+                            }
+                        },
+                        data: ["3060", "3050", "3800.1", "3837.3", "3757.3", "3010.5", "3040", "3060", "3100", "3050.00", "3050", "3050", 0, 0, 0, "3373.00", "3073.00", "3600", "3603", "3600", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "3600", "3700.00", 0, "3700", "3700", 0, "3700", "3700", "3700", "3821.00", "3822.11", "3822.11", "3500", "4111", "3555", "3555", 0, 0, 0, "3555", "3555", 0, "3555", "3555", "3555", "3580", "3580", 0, "3580"]
+                    },]
+                },
             }
         },
         methods: {
@@ -228,16 +445,32 @@
                 }
                 return res;
             },
+            mapClickEvent() {
+                let innerChart;
+                let _this = this;
+                if (this.showMapChart) {
+                    this.showMapChart = false;
+
+                } else {
+                    this.showMapChart = true;
+                    setTimeout(function () {
+                        innerChart = echarts.init(document.getElementById('map-innner-chart'));
+                        innerChart.setOption(_this.chartOption);
+                    }, 500);
+                }
+
+
+            },
             drawLine() {
                 // 基于准备好的dom，初始化echarts实例
                 let myChart = echarts.init(document.getElementById('transaction-map'));
-                /*
-                                myChart.on('click', function (param) {
-                                    console.log(param)
-                                    if (param.componentType == 'geo') {
-                                        this.mapClickEvent();
-                                    }
-                                });*/
+                let _this = this;
+                myChart.on('click', function (param) {
+                    console.log(param)
+                    if (param.componentType == 'geo') {
+                        _this.mapClickEvent();
+                    }
+                });
 
                 // 绘制图表
                 myChart.setOption({
