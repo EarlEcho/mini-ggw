0a516b8e5bf88d95043ed1f3c1f1716c8b5d8263
EarEcho <734952325@qq.com>
2017-12-24 14:59:31 +0800

    增加视频页面布局 以及视频播放插件

--- src/views/cloudStorage.vue
+++ src/views/cloudStorage.vue
@@ -1,10 +1,36 @@
 <!--云仓储-->
 <style scoped lang="less">
-
+    .cloud-wrapper{
+        border: solid 1px red;
+        width: 1920px;
+        height: 950px;
+        -webkit-box-sizing: border-box;
+        box-sizing: border-box;
+        position: absolute;
+        padding: 0 35px;
+        .cloud-left-wrapper{
+            width: 322px;
+            height: 914px;
+            border: solid 1px red;
+            float: left;
+            margin-right: 30px;
+        }
+        .cloud-right-wrapper{
+            width: 1492px;
+            height: 914px;
+            border: solid 1px red;
+            float: left;
+        }
+    }
 </style>
 <template>
     <div class="cloud-wrapper">
-        云仓储
+        <div class="cloud-left-wrapper">
+
+        </div>
+        <div class="cloud-right-wrapper">
+
+        </div>
     </div>
 </template>
 
